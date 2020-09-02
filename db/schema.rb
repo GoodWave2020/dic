716564ebbe7284e3490c84e70cd2b8aa689f1392
BachShinichiroe <kitchen.number@gmail.com>
2020-09-02 20:54:40 +0900

    ログイン機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -12,16 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2020_09_02_081449) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
